2706606557b6d3caf23c304b39ee67824d67a906
honeybee <1002213567@qq.com>
2015-05-24 17:39:47 +0800

    to prove i'm alive

--- gif people/gif.js
+++ gif people/gif.js
@@ -1,24 +1,25 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("canvas") ;
 var context = canvas.getContext("2d");
+var section = $(".canvas");
 
-function GIF(speed,imgName,imgNumber,imgFormat){
+function GIF(location,speed){
 	var IMG = new Array();
-	for (var i = 0; i < imgNumber; i++) {
+	var WIDTH = section.width();
+	var HEIGHT = section.height();
+
+	var step = parseInt(location * WIDTH / (15 * speed) );
+	var j = 0;
+
+	for (var i = 0; i < 18; i++) {
 		var img = new Image();
-		img.src = "img/" + imgName + i + "." +imgFormat;
+		img.src = "img/people" + i + ".png";
 		IMG.push(img);
 	}
-	console.log(IMG);
+
 	var Obj = {
 		Stop : document.getElementById("stop"),
 		Start : document.getElementById("start"),
 		flag : 0
-	}
-
-	window.onload = function(){
-		stop();
-		start();
-		loop();
 	}
 
 	function loop(){
@@ -26,31 +27,33 @@ function GIF(speed,imgName,imgNumber,imgFormat){
 		Obj.flag = setInterval(function(){
 			update();
 			drawPic();
-		},1000/speed);
+		},1000/15);
 	}
 
+	loop();
+
 	function drawPic(){
-		context.clearRect(0,0,400,600);
-		context.drawImage(IMG[i],50,50);
+		if (j * step < location * WIDTH) {
+			context.clearRect(0,0,400,600);
+			context.drawImage(IMG[i],j * step ,0,HEIGHT / 1.5,HEIGHT);
+		}else{
+			clearInterval(Obj.flag);
+			context.drawImage(IMG[17],j * step ,0,HEIGHT / 1.5, HEIGHT);
+		}
+		
 	}
 
 	function update(){
+		WIDTH = section.width();
+		HEIGHT = section.height();
+		canvas.width = WIDTH;
+		canvas.height = HEIGHT;
 		i += 1;
-		if (i >= imgNumber) {
+		j += 1;
+		if (i >= 18 - 1) {
 			i = 0;
 		}
 	}
 
-	function stop(){
-		Obj.Stop.onclick=function(){
-			clearInterval(Obj.flag);
-		}
-	}
-	function start(){
-		Obj.Start.onclick=function(){
-			loop();
-		}
-	}
 }
-
-GIF(6,"",13,"png"); 
+GIF(0.3,5);
