f675619d49a08564518639925e07d567adf4a35e
honeybee <1002213567@qq.com>
2015-09-12 16:19:50 +0800

    updata

--- ajax2/upload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	/*本demo暂不支持中文文件名。*/
-	$fileName = $_POST['fileName']; 
- 	file_put_contents('uploads/'.$fileName, file_get_contents($_FILES["filePart"]["tmp_name"]), FILE_APPEND);
-	echo json_encode(['status' => 200]);
